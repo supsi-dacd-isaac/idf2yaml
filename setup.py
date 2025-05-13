8b9f3e2fc211050ddbbfd7241646722700f58d93
Federico Rosato <33623325+f-rosato@users.noreply.github.com>
2025-05-13 16:56:13 +0200

    iddfiles moved in core
    the /iddfiles folder, with the default idd file, was moved inside /core for more straightforward deployability

--- setup.py
+++ setup.py
@@ -3,4 +3,7 @@ from setuptools import setup, find_packages
 setup(
     packages=find_packages(exclude=["tests"]),
     include_package_data=True,
+    package_data={
+        "idf2yaml": ["iddfiles/*"],
+    },
 )
